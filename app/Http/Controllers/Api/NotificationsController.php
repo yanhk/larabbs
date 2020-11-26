4ead651e49ea93949b673ed214c129a6e779446f
yanhk <705177961@qq.com>
2020-11-26 20:51:07 +0800

    消息已读 1126

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -8,10 +8,26 @@ use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
-    //
+    // 消息通知列表
     public function index(Request $request)
     {
         $notifications = $request->user()->notifications()->paginate();
         return NotificationResource::collection($notifications);
+    }
+
+    // 未读消息统计
+    public function stats(Request $request)
+    {
+        return response()->json([
+            'unread_count' => $request->user()->notification_count
+        ]);
+    }
+
+    // 标记消息为已读
+    public function read(Request $request)
+    {
+        $request->user()->markAsRead();
+
+        return response(null, 204);
     }
 }
