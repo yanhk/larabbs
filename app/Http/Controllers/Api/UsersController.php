d7b3c53c6f5a53bec3f97efad41ded2eda21088d
yanhk <705177961@qq.com>
2020-11-26 21:50:36 +0800

    活跃用户

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -79,4 +79,12 @@ class UsersController extends Controller
 
         return (new UserResource($user))->showSensitiveFields();
     }
+
+
+    // 获取活跃用户
+    public function activedIndex(User $user)
+    {
+        UserResource::wrap('data');
+        return UserResource::collection($user->getActiveUsers());
+    }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,17 +11,17 @@ class UsersTableSeeder extends Seeder
         $users = factory(User::class)->times(10)->create();
 
         // 单独处理第一个用户的数据
-        $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
-        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
-        $user->save();
+//        $user = User::find(1);
+//        $user->name = 'Summer';
+//        $user->email = '123@163.com';
+//        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+//        $user->save();
 
         // 初始化用户角色，将 1 号用户指派为『站长』
-        $user->assignRole('Founder');
+//        $user->assignRole('Founder');
 
         // 将 2 号用户指派为『管理员』
-        $user = User::find(2);
-        $user->assignRole('Maintainer');
+//        $user = User::find(2);
+//        $user->assignRole('Maintainer');
     }
 }
